a214248938ad1d7ed8bbb4f3048e43ac0dbe96cd
Joshua Needham <joshuabneedham@gmail.com>
2017-11-23 19:11:30 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -5,7 +5,21 @@ class Triangle
   def initialize(length_one, length_two, length_three)
   end
 
-  def kind
-  end
+  def kind(a, b, c)
+
+    s = (a + b + c) / 2.0
+
+    ok = (s - a) * (s - b) * (s - c)
+
     
+    if a == b && b == c then
+      :equilateral
+    elsif a == b || a == c || b == c then
+      :isosceles
+    else
+      :scalene
+    end
+  end
+  end
+
 end
