6806470466da8d262202c0fe89918b0c7566640f
Joshua Needham <joshuabneedham@gmail.com>
2017-11-23 19:11:44 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,15 +1,15 @@
 class Triangle
   # write code here
   attr_accessor :equilateral, :isosceles, :scalene
-  
+
   def initialize(length_one, length_two, length_three)
   end
 
   def kind(a, b, c)
 
-    s = (a + b + c) / 2.0
-
-    ok = (s - a) * (s - b) * (s - c)
+    # s = (a + b + c) / 2.0
+    # 
+    # ok = (s - a) * (s - b) * (s - c)
 
     
     if a == b && b == c then
